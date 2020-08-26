2af565e6f8671069b01c3c9295aac120ffb8549e
pavstar619 <52383535+pavstar619@users.noreply.github.com>
2020-08-26 09:24:19 +0600

    changes

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 import numpy as np
 import pandas as pd
 from flask import Flask, request, jsonify, render_template
-import pickle
 
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
+@app.route('/index')
 def home():
     return render_template('index.html')
 
